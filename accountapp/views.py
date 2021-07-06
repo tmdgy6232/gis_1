5dd9a74680511f53a92c1b366bd40cca908b7d42
seunghyo <tmdgy6232@naver.com>
2021-07-06 16:16:13 +0900

    7/6 model create

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from accountapp.models import HelloWorld
+
 
 def helloworld(request):
     return render(request, 'accountapp/hello_world.html')
