e1d0d5515fe7b888a55cc7b325de33911d28c1b3
seunghyo <tmdgy6232@naver.com>
2021-07-13 14:42:26 +0900

    7/13 createView 작성 및 url 연결

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 
-from accountapp.views import helloworld
+from accountapp.views import helloworld, AccountCreateView
 
 app_name = 'accountapp'
 
 urlpatterns = [
     path('hello_world/', helloworld, name = 'hello_world'),
+    path('create/', AccountCreateView.as_view(), name='create')
 ]
