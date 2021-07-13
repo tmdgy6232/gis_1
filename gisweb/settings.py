efdde746abbcc18c2c168c7b9abfd0f1cae2182d
seunghyo <tmdgy6232@naver.com>
2021-07-13 16:14:05 +0900

    Login, Logout nav 추가 and RedicrectUrl 설정

--- gisweb/settings.py
+++ gisweb/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from django.urls import reverse_lazy
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 local_env = open(os.path.join(BASE_DIR,'.env'))
@@ -142,3 +144,6 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+
+LOGOUT_REDIRECT_URL = 'accountapp:login'
