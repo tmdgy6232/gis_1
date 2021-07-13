a3b407e0d8b5ceccc12e05d7a8e27d2e485a31da
seunghyo <tmdgy6232@naver.com>
2021-07-13 16:08:05 +0900

    account create 생성

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 
 from accountapp.views import helloworld, AccountCreateView
@@ -6,5 +7,7 @@ app_name = 'accountapp'
 
 urlpatterns = [
     path('hello_world/', helloworld, name = 'hello_world'),
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('create/', AccountCreateView.as_view(), name='create')
 ]
