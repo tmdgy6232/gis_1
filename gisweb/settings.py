8a9eea3ddcf408d670e7bb998475c647421f0995
tmdgy6232 <tmdgy6232@naver.com>
2021-07-27 14:54:26 +0900

    7/27 form_vaild 사용해서 profileapp 유호성 체크부분 추가

--- gisweb/settings.py
+++ gisweb/settings.py
@@ -51,8 +51,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accountapp',
-    'bootstrap4',
     'profileapp',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
