98dc090635a3da8e7ffa9b4b2ee82d501bcac076
tmdgy6232 <tmdgy6232@naver.com>
2021-07-29 09:39:05 +0900

    7/27 profileapp decorator 사용하여 인증 적용

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-from django.urls import reverse_lazy, reverse
+from django.urls import reverse
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
 from profileapp.forms import ProfileCreationForm
+from profileapp.decorators import profile_ownership_required
 from profileapp.models import Profile
 
-
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
 class ProfileCreateView(CreateView):
     model = Profile
     form_class = ProfileCreationForm
@@ -21,6 +24,8 @@ class ProfileCreateView(CreateView):
     def get_success_url(self):
         return reverse('accountapp:detail', kwargs={'pk':self.object.user.pk})
 
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
 class ProfileUpdateView(UpdateView):
     model = Profile
     context_object_name = 'target_profile'
