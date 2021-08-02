57d8b019b0a8b0e536e542a1f2f91462a45d6186
tmdgy6232 <tmdgy6232@naver.com>
2021-08-02 09:16:31 +0900

    8/2 article app List magicgrid 적용

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
+from django.views.generic import TemplateView
 
+app_name = 'articleapp'
+#templateView = 로직없이 어떤 html을 쓸건지 말해주면 템플릿만 보여주는 기능을 함
 urlpatterns = [
-
+    path('list', TemplateView.as_view(template_name='articleapp/list.html'),name='list')
 ]
 
