2775b3778767e277af89221ef9664a7636f5f9b9
tmdgy6232 <tmdgy6232@naver.com>
2021-08-03 15:14:11 +0900

    8/3 article detail 기능 추가 (디자인 전)

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from django.views.generic import TemplateView
 
-from articleapp.views import ArticleCreateView
+from articleapp.views import ArticleCreateView, ArticleDetailView
 
 app_name = 'articleapp'
 
 urlpatterns = [
     path('list/', TemplateView.as_view(template_name='articleapp/list.html'),name='list'), # templateView = 로직없이 어떤 html을 쓸건지 말해주면 템플릿만 보여주는 기능을 함
-    path('create/', ArticleCreateView.as_view(), name='create')
+    path('create/', ArticleCreateView.as_view(), name='create'),
+    path('detail/<int:pk>', ArticleDetailView.as_view(), name='detail'),
 ]
 
