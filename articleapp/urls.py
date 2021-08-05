3ed50f73003d9652b4dec6f8571ec618f27baf8c
tmdgy6232 <tmdgy6232@naver.com>
2021-08-05 10:23:01 +0900

    8/5 ariticleapp list 추가 (ListVIew 사용 ps. pagenation )

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path, include
 from django.views.generic import TemplateView
 
-from articleapp.views import ArticleCreateView, ArticleDetailView, ArticleUpdateView, ArticleDeleteView
+from articleapp.views import ArticleCreateView, ArticleDetailView, ArticleUpdateView, ArticleDeleteView, ArticleListView
 
 app_name = 'articleapp'
 
 urlpatterns = [
-    path('list/', TemplateView.as_view(template_name='articleapp/list.html'),name='list'), # templateView = 로직없이 어떤 html을 쓸건지 말해주면 템플릿만 보여주는 기능을 함
+    path('list/', ArticleListView.as_view(), name='list'),  # templateView = 로직없이 어떤 html을 쓸건지 말해주면 템플릿만 보여주는 기능을 함
     path('create/', ArticleCreateView.as_view(), name='create'),
     path('detail/<int:pk>', ArticleDetailView.as_view(), name='detail'),
     path('update/<int:pk>', ArticleUpdateView.as_view(), name='update'),
     path('delete/<int:pk>', ArticleDeleteView.as_view(), name='delete'),
 ]
-
