45200f688e270e45d3135cee83e83f27121b8fcd
tmdgy6232 <tmdgy6232@naver.com>
2021-08-30 10:12:49 +0900

    8/30 불필요 부분 수정, 헤더 디자인 및 홈 루트 수정, article model blank 기능 추가

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 
-from accountapp.views import helloworld, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
+from accountapp.views import AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 
 app_name = 'accountapp'
 
